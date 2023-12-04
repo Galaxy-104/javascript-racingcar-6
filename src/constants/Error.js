8f14e309e4cd820519bc09a587efa2b0c7a60854
Galaxy <js_park95@naver.com>
2023-12-04 11:51:48 +0900

    feat: 게임 기본 값 설정
    안내 메시지, 에러 메시지, 게임 값 초기 설정

---
+++ src/constants/Error.js
@@ -0,0 +1,18 @@
+const common = Object.freeze({
+  blank: "값을 입력해주세요.",
+})
+
+const name = Object.freeze({
+  letter: "자동차의 이름은 한글, 영어, 숫자만 가능합니다.",
+  length: "이름은 5글자 이하로 입력해주세요.",
+  lengthNumber: 5,
+})
+
+const repeat = Object.freeze({
+  notANumber: "입력 값이 숫자가 아닙니다.",
+  natural: "1 이상의 자연수를 입력해주세요.",
+})
+
+export const ERROR = Object.freeze({
+  common, name, repeat
+})
---
+++ src/constants/Racing.js
@@ -0,0 +1,19 @@
+const input = Object.freeze({
+  car: "경주할 자동차 이름을 입력하세요.(이름은 5글자 이하, 쉼표(,) 기준으로 구분)",
+  repeat: "시도할 횟수는 몇 회인가요?",
+})
+
+const output = Object.freeze({
+  result: "실행 결과",
+  winner: "최종 우승자 : ",
+})
+
+const game = Object.freeze({
+  forward: 4,
+  min: 0,
+  max: 9,
+})
+
+export const RACING = Object.freeze({
+  input, output, game
+})
